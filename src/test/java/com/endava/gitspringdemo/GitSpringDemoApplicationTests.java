5d6eb1980c81accdf8bec8233b071419f27947ff
catalinhalic <catalin.halic@endava.com>
2025-12-04 11:20:52 +0200

    Setup Spring Project

---
+++ src/test/java/com/endava/gitspringdemo/GitSpringDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.endava.gitspringdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitSpringDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
